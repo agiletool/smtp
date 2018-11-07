2903d674eb01382bbf474522b3419979661835cf
root <root@ip-172-31-28-75.ec2.internal>
2018-11-07 09:42:13 +0000

    new branch

--- rootfs/etc/dovecot/sieve/rspamd-pipe-ham.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# rspamd client reads piped ham message from the standard input
-exec /usr/bin/rspamc learn_ham
--- rootfs/etc/dovecot/sieve/rspamd-pipe-spam.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# rspamd client reads piped spam message from the standard input
-exec /usr/bin/rspamc learn_spam
